a6ea12334ce2675c00ce95ff7340876a78ad8660
cuongdqpayment <cuongdq.payment@gmail.com>
2018-12-11 13:33:52 +0700

    cuongdq push by sh

--- gitc3-push.sh
+++ gitc3-push.sh
@@ -1,8 +1,10 @@
-git init
+#git init
 git add .
 git commit -m "cuongdq push by sh"
 #git remote -v
 #git remote remove gitc3 
-#(thay chu origin = gitc3 de quan ly de hon)
+#git remote add gitc3 cuongdq@10.151.54.84:/mnt/gitserver/c3-oauth.git
+#(thay chu origin = gitc3 de quan ly de hon )
+#(neu da co add roi thi chi can set-url la duoc)
 #git remote set-url gitc3 cuongdq@10.151.54.84:/mnt/gitserver/c3-oauth.git
 git push -u gitc3 master
