250146443789575e584b05944c76604c4cddc84c
cuongdq.payment <cuongdq.payment@gmail.com>
2019-02-12 11:25:08 +0700

    cuongdq push by sh

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,139 @@
+import { Component } from '@angular/core';
+import { NavController, ModalController, Platform, AlertController, LoadingController } from 'ionic-angular';
+import { DynamicFormWebPage } from '../dynamic-form-web/dynamic-form-web';
+import { DynamicListPage } from '../dynamic-list/dynamic-list';
+import { ApiResourceService } from '../../services/apiResourceServices';
+import { DynamicFormMobilePage } from '../dynamic-form-mobile/dynamic-form-mobile';
+import { DynamicCardSocialPage } from '../dynamic-card-social/dynamic-card-social';
+import { DynamicMediasPage } from '../dynamic-medias/dynamic-medias';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+  
+  tabs: any = [
+    {
+      root: DynamicFormWebPage,
+      title: 'Form',
+      icon: 'log-in'
+    },
+    {
+      root: DynamicFormMobilePage,
+      title: 'Mobile',
+      icon: 'phone-portrait'
+    },
+    {
+      root: DynamicMediasPage,
+      title: 'Images',
+      icon: 'images'
+    },
+    {
+      root: DynamicCardSocialPage,
+      title: 'Social',
+      icon: 'today'
+    },
+    {
+      root: DynamicCardSocialPage,
+      title: 'List',
+      icon: 'paper'
+    }
+  ];
+
+  constructor(private navCtrl: NavController
+            , private modalCtrl: ModalController
+            , private platform: Platform
+            , private alertCtrl: AlertController
+            , private loadingCtrl: LoadingController
+            , private resource: ApiResourceService
+  ) {
+    //console.log('1. constructor tabs')
+  }
+  
+  ngOnInit(){
+    //console.log('2. ngOnInit tabs')
+
+    this.tabs[0].params = {
+      step:'bill-cycle',
+      callback: this.callback,
+      call_waiting_data: this.callWaiting
+    }
+
+  }
+
+  ionViewDidLoad() {
+    //console.log('3. ionViewDidLoad tabs')
+  }
+
+  ionViewWillEnter(){
+    //console.log('4. ionViewWillEnter tabs')
+  }
+
+  callback(){
+
+  }
+
+  callWaiting(){
+
+  }
+
+  
+  openModal(data) {
+
+    return new Promise( (resolve, reject) => { 
+      let formPopup:any;
+     
+      if (this.platform.is('core')){
+        formPopup = DynamicFormWebPage;
+      }else{
+        formPopup = DynamicFormMobilePage;
+      }
+
+      let modal = this.modalCtrl.create(formPopup, data);
+          modal.onDidDismiss(data=>{
+            resolve(data);
+          })
+      modal.present();
+    })
+    
+  }
+
+
+  presentConfirm(jsonConfirm:{title:string,message:string,cancel_text:string,ok_text:string,ok:any}) {
+    let alert = this.alertCtrl.create({
+      title: jsonConfirm.title, //'Xác nhận phát hành',
+      message: jsonConfirm.message, //'Bạn muốn ',
+      buttons: [
+        {
+          text: jsonConfirm.cancel_text, //Bỏ qua
+          role: 'cancel',
+          handler: () => {
+            if (jsonConfirm.ok) jsonConfirm.ok(false);
+          }
+        },
+        {
+          text: jsonConfirm.ok_text,//'Đồng ý',
+          handler: () => {
+            if (jsonConfirm.ok) jsonConfirm.ok(true);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  presentAlert(jsonConfirm:{title:string,message:string,ok_text:string}) {
+    let alert = this.alertCtrl.create({
+      title: jsonConfirm.title, //'Xác nhận phát hành',
+      message: jsonConfirm.message, //'Bạn muốn ',
+      buttons: [
+        {
+          text: jsonConfirm.ok_text,//'Đồng ý',
+          handler: () => {}
+        }
+      ]
+    });
+    alert.present();
+  }
+
+}
